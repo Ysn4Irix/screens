978bec6e74027f529b96278a642c1084a7be741c
Ysn4Irix <80527916+Ysn4Irix@users.noreply.github.com>
2023-05-21 17:05:06 +0100

    :fire: changing adapter from vercel to node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import vercel from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,11 +9,7 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: vercel({
-			edge: false,
-			external: [],
-			split: false
-		}),
+		adapter: adapter(),
 		alias: {
 			$src: './src'
 		}
